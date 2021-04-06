29ccc27883ab3e6bb126870f2c1ed0e3c4562f08
Ruben Spolmink <ruben.spolmink@gmail.com>
2021-04-06 15:11:51 +0200

    Add files via upload
    bash file that removes noise, normalize and filters out the silence parts in the audio files

---
+++ denoise.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for id in */ ; do
+    #echo "$id"
+  for path in $id* ; do
+    #echo "$path
+    COUNTER=0
+    for file in $path/*.wav ; do
+      #echo "/$file"
+      #echo "$COUNTER"
+      yes | ffmpeg -i $file -af "anlmdn" $path/${COUNTER}_no_noise.wav
+      yes | ffmpeg -i $path/${COUNTER}_no_noise.wav -af "loudnorm" $path/${COUNTER}_normalized.wav
+      yes | ffmpeg -i $path/${COUNTER}_normalized.wav -af silenceremove=1:0:-50dB $path/${COUNTER}.wav
+      rm $file
+      rm $path/${COUNTER}_no_noise.wav
+      rm $path/${COUNTER}_normalized.wav
+      COUNTER=$((COUNTER + 1))
+    done
+  done
+done
