d69bcf040fe5567faf35a23c9e0b64af60ced7a9
Ruben Spolmink <ruben.spolmink@gmail.com>
2021-04-06 15:15:03 +0200

    Add files via upload
    small bug fix

--- denoise.sh
+++ denoise.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-for id in */ ; do
+for nationality in */ ; do
+  for id in $nationality* ; do
     #echo "$id"
-  for path in $id* ; do
+    for path in $id* ; do
     #echo "$path
-    COUNTER=0
-    for file in $path/*.wav ; do
+      COUNTER=0
+      for file in $path/*.wav ; do
       #echo "/$file"
       #echo "$COUNTER"
-      yes | ffmpeg -i $file -af "anlmdn" $path/${COUNTER}_no_noise.wav
-      yes | ffmpeg -i $path/${COUNTER}_no_noise.wav -af "loudnorm" $path/${COUNTER}_normalized.wav
-      yes | ffmpeg -i $path/${COUNTER}_normalized.wav -af silenceremove=1:0:-50dB $path/${COUNTER}.wav
-      rm $file
-      rm $path/${COUNTER}_no_noise.wav
-      rm $path/${COUNTER}_normalized.wav
-      COUNTER=$((COUNTER + 1))
+        yes | ffmpeg -i $file -af "anlmdn" $path/${COUNTER}_no_noise.wav
+        yes | ffmpeg -i $path/${COUNTER}_no_noise.wav -af "loudnorm" $path/${COUNTER}_normalized.wav
+        yes | ffmpeg -i $path/${COUNTER}_normalized.wav -af silenceremove=1:0:-50dB $path/${COUNTER}.wav
+        rm $file
+        rm $path/${COUNTER}_no_noise.wav
+        rm $path/${COUNTER}_normalized.wav
+        COUNTER=$((COUNTER + 1))
+      done
     done
   done
 done
